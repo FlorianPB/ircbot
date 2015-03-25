84e55a4ff18310059ead68e62cdd77503350c837
Adrien Sohier <adrien.sohier@art-software.fr>
2015-03-25 19:01:33 +0100

    Send log method instead of whole object

--- net/connect.py
+++ net/connect.py
@@ -5,30 +5,30 @@ import socket
 import util.log
 
 class Connect:
-    def __init__(self, logInstance, srv="chat.freenode.net", port=6667):
+    def __init__(self, logMethod, srv="chat.freenode.net", port=6667):
         self.srv = srv
         self.port = port
-        self.log = logInstance
+        self.log = logMethod
         self.socket = None
 
     def start(self):
         """Starts the connection with available info"""
         if self.socket == None:
-            self.log.log("Connecting socket to %s:%d" % (self.srv, self.port), "net.connect.Connect.start", util.log.INFO)
+            self.log("Connecting socket to %s:%d" % (self.srv, self.port), "net.connect.Connect.start", util.log.INFO)
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.socket.connect((self.srv, self.port))
 
     def stop(self):
         """Stops the current connection"""
         if self.socket != None:
-            self.log.log("Closing connection", "net.connect.Connect.stop", util.log.INFO)
+            self.log("Closing connection", "net.connect.Connect.stop", util.log.INFO)
             self.socket.close()
             self.socket = None
 
     def sendText(self, text, encoding="utf8"):
         """Sends text to the connection"""
         if self.socket != None:
-            self.log.log(text, "net.connect.Connect.sendText", util.log.DEBUG)
+            self.log(text, "net.connect.Connect.sendText", util.log.DEBUG)
             self.socket.send(bytes(text, encoding))
 
     def waitText(self, encoding="utf8", bufSize=(2**16)-1):
@@ -36,6 +36,6 @@ class Connect:
         txt = ""
         if self.socket != None:
             txt = str(self.socket.recv(bufSize), encoding)
-            self.log.log(txt, "net.connect.Connect.waitText", util.log.DEBUG)
+            self.log(txt, "net.connect.Connect.waitText", util.log.DEBUG)
 
         return txt
