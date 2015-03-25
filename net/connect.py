5142fd260df3ff881de84e8a5d99c1e470aa0093
Adrien Sohier <adrien.sohier@art-software.fr>
2015-03-25 18:20:36 +0100

    log and connect base modules

---
+++ net/connect.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+import socket
+
+class Connect:
+    def __init__(self, srv="chat.freenode.net", port=6667, channel="#bronycub"):
+        self.srv = srv
+        self.port = port
+        self.channel = channel
+        self.socket = None
+
+    def start(self):
+        """Starts the connection with available info"""
+        if self.socket == None:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.connect((self.srv, self.port))
+
+    def stop(self):
+        """Stops the current connection"""
+        if self.socket != None:
+            self.socket.close()
+            self.socket = None
+
+    def sendText(self, text, encoding="utf8"):
+        """Sends text to the connection"""
+        if self.socket != None:
+            self.socket.send(bytes(text, encoding))
+
+    def waitText(self, encoding="utf8", bufSize=(2**16)-1):
+        """Wait for text from the connection"""
+        txt = ""
+        if self.socket != None:
+            txt = str(self.socket.recv(bufSize), encoding)
+
+        return txt
---
+++ util/log.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+from time import strftime
+from sys import stdout,stderr
+
+DEBUG    = 0
+INFO     = 1
+NOTIF    = 2
+WARNING  = 3
+ERROR    = 4
+CRIRITAL = 5
+
+textLevels = {
+        0:"DEBUG",
+        1:"INFOR",
+        2:"NOTIF",
+        3:" WARN",
+        4:"ERROR",
+        5:" CRIT"
+}
+
+class Log:
+    handle = None,
+    path = "/tmp/ircbot.log"
+    stdoutLogLevel = NOTIF
+    stderrLogLevel = WARNING
+
+    def __init__(self, fp, stdout_l=NOTIF, stderr_l=WARNING):
+        """Initializes logger"""
+        self.stdoutLogLevel = stdout_l
+        self.stderrLogLevel = stderr_l
+        self.path = fp
+
+        self.handle = open(self.path, "w")
+
+    def log(self, content, head="logger", level=INFO):
+        """Outputs a log line"""
+
+        strLog = "[%s] %s %s: %s\n" % (strftime("%Y-%m-%d %H:%M:%S"), textLevels[level], head, content)
+
+        self.handle.write(strLog)
+        self.handle.flush()
+        
+        if level>=self.stderrLogLevel:
+            stderr.write(strLog)
+            stderr.flush()
+        elif level>=self.stdoutLogLevel:
+            stdout.write(strLog)
+            stdout.flush()
+
+    def close(self):
+        """Close log system"""
+
+        self.handle.close()
