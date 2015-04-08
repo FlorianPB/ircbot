91cc097c84a3992b07216991db0affb5d1631360
Adrien Sohier <adrien.sohier@art-software.fr>
2015-04-08 09:05:21 +0200

    cfg: add external cfg to ircbot.py
    ircbot.py now takes it's main config vars from util.cfg
    This applies for the following:
    - server address & port
    - channels to join
    - identication information (/!\ no NickServ auth yet

--- ircbot.py
+++ ircbot.py
@@ -2,21 +2,28 @@
 # -*- coding: utf8 -*-
 
 import util.log
+import util.cfg
 import net.connect
 import net.irc
 
+
 def runOnce():
+    """Runs the bot once"""
+    cfg = util.cfg.load()
+
     logger = util.log.Log("bot.log", file_l=util.log.DEBUG, stdout_l=util.log.DEBUG, stderr_l=util.log.WARNING)
     logger.log("Started log", "init.bot", util.log.NOTIF)
 
-    connectHandler = net.connect.Connect(logger.log, "chat.freenode.net", 6667)
+    connectHandler = net.connect.Connect(logger.log, cfg["srv"], cfg["port"])
     connectHandler.start()
     logger.log("TCP Connection initialized", "init.bot", util.log.INFO)
 
     logger.log("IRC Server identication...", "init.bot", util.log.INFO)
-    ircHandler = net.irc.IRC("jamesthebot", connectHandler, logger.log, "jamesthebot", "James the nice bot")
+    ircHandler = net.irc.IRC(cfg["nick"], connectHandler, logger.log, cfg["username"], cfg["realname"])
     ircHandler.ident()
-    ircHandler.join("#testjamesthebot")
+
+    for chan in cfg["channels"]:
+        ircHandler.join(chan)
 
     dontStop = True
 
