5eac1c38324e4a85efe33cb35f37f0799e86da80
Adrien Sohier <adrien.sohier@art-software.fr>
2015-04-10 19:33:10 +0200

    added nickserv ident

---
+++ events/01-nickserv.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- codimg: utf8 -*-
+"""Identify to nickserv if needed"""
+
+initData = {}
+
+def init(data):
+    global initData
+
+    initData = data
+    data["irc"].hooks["NOTICE"].append(nickservIdent)
+
+def nickservIdent(evt):
+    """Get nickserv password"""
+    evt[0].lower() == ":nickserv!nickserv@services."
+    if " ".join(evt[3:7]).lower() == ":this nickname is registered":
+        pw = input("NickServ password for " + evt[2] + " : ")
+        initData["connect"].sentText("PRIVMSG NickServ :IDENTIFY " + pw + "\r\n")
+        del pw
