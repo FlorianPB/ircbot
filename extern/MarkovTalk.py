4b881c1f1acc4cc70ec9d14e03dc92b6c504103b
Adrien Sohier <adrien.sohier@art-software.fr>
2015-06-28 14:21:29 +0200

    Lastnode

--- extern/MarkovTalk.py
+++ extern/MarkovTalk.py
@@ -8,35 +8,42 @@ import pickle
 import util.cfg
 util.cfg.default = {"order": 2, "randomWalk": True}
 
-context = [-1,] * 5
-phrases = []
-phraseList = {}
 mots = {}
+lastNode = ""
 cfg = util.cfg.default
 
 def initDb():
     """Loads the DB"""
-    global phrases, phraseList, mots, cfg
+    global mots, cfg
 
     cfg = util.cfg.load("markov.json")
 
-    if os.path.isfile("phrases"):
-        with open("phrases", "rb") as f:
-            phrases = pickle.load(f)
-    
-    if os.path.isfile("nodes-phrases"):
-        with open("nodes-phrases", "rb") as f:
-            phraseList = pickle.load(f)
-    
     if os.path.isfile("mots"):
         with open("mots", "rb") as f:
             mots = pickle.load(f)
 
-def computeRandomSentence():
+def computeRandomSentence(startFromLast=False):
     """Computes a random sentence"""
-    starts = [i for i in mots.keys() if i[0:4]=="END|"]
-    
-    ph = starts[random.randint(0,len(starts)-1)]
+    global lastNode
+
+    # Decides which node to start from
+    if startFromLast and lastNode!="" and mots.__contains__(lastNode) and len(mots[lastNode])>0:
+        if cfg["randomWalk"] == True:
+            lst = list(mots[lastNode].keys())
+            ph = lst[random.randint(0, len(lst)-1)]
+        else:
+            total = sum([mots[lastNode][i] for i in mots[lastNode].keys()])
+            baseValue = 0
+            value = random.randint(0, total - 1)
+            for candidat in mots[lastNode].keys():
+                if baseValue <= value and mots[lastNode][candidat] > value:
+                    ph = candidat
+                    break
+                baseValue += mots[lastNode][candidat]
+
+    else:
+        starts = [i for i in mots.keys() if i[0:4]=="END|"]
+        ph = starts[random.randint(0,len(starts)-1)]
     
     while ph[-4:]!="|END":
         lst = mots["|".join(ph.split("|")[-cfg["order"]:])]
@@ -51,15 +58,18 @@ def computeRandomSentence():
                     ph += "|" + candidat
                     break
                 baseValue += lst[candidat]
+
+    lastNode="|".join(ph.split("|")[-cfg["order"]:])
     
     return " ".join(ph.split("|")[1:-1])
 
 def AnalyzeSentence(phrase):
     """Analyzes a sentence"""
-    global mots
+    global mots, lastNode
     
     p = ["END",] + phrase.split() + ["END",]
-    
+   
+    # Analyze each group of words
     for i in range(len(p)-cfg["order"]):
         node = "|".join(p[i:i+cfg["order"]])
         if mots.__contains__(node):
@@ -69,6 +79,18 @@ def AnalyzeSentence(phrase):
                 mots[node][p[i+cfg["order"]]] = 1
         else:
             mots[node] = {p[i+cfg["order"]]: 1}
+
+    # Update lastNode stats to reflect lastNode -> current sentence link weight
+    if mots.__contains__(lastNode):
+        if mots[lastNode].__contains__(p[0:cfg["order"]]):
+            mots[lastNode][p[0:cfg["order"]]] += 1
+        else:
+            mots[lastNode][p[0:cfg["order"]]] = 1
+    else:
+        mots[lastNode] = {p[0:cfg["order"]]]: 1}
+
+    # Update lastNode to what the user just said
+    lastNode = "|".join(p[-cfg["order"]:])
     
     with open("mots", "wb") as f:
         pickle.dump(mots, f)
@@ -76,7 +98,7 @@ def AnalyzeSentence(phrase):
 def AnalyseFile(filename):
     """Analyzes a file"""
     text = open(filename, "r")
-    global context, phrases, phraseList, mots, context
+    global mots, lastNode
     
     for lineId, phrase in enumerate(text.readlines()):
         print("Analyzing line %d..." % lineId, end="\x1B[0K\r")
@@ -95,116 +117,26 @@ def AnalyseFile(filename):
                     mots[node][p[i+cfg["order"]]] = 1
             else:
                 mots[node] = {p[i+cfg["order"]]: 1}
-        
-        # Analyse de chaque phrase, et de leurs relation
-        if not phrases.__contains__(phrase):
-            phrases.append(phrase)
-        
-        idx = phrases.index(phrase)
-        context = context[1:] + [idx,]
-        for i in range(5):
-            c = "%d|%d|%d|%d|%d" % tuple(([-1,]*5)[0:i] + context[i:5])
-            
-            if c != "-1|-1|-1|-1|-1":
-                if phraseList.__contains__(c):
-                    if phraseList[c].__contains__(idx):
-                        phraseList[c][idx] += 1
-                    else:
-                        phraseList[c][idx] = 1
-                else:
-                    phraseList[c] = {idx:1}
     
+        if mots.__contains__(lastNode):
+            if mots[lastNode].__contains__(p[0:cfg["order"]]):
+                mots[lastNode][p[0:cfg["order"]]] += 1
+            else:
+                mots[lastNode][p[0:cfg["order"]]] = 1
+        else:
+            mots[lastNode] = {p[0:cfg["order"]]]: 1}
+
+        lastNode = "|".join(p[-cfg["order"]:])
+        
     with open("mots", "wb") as f:
         pickle.dump(mots, f)
-    
-    with open("nodes-phrases", "wb") as f:
-        pickle.dump(phraseList, f)
-    
-    with open("phrases", "wb") as f:
-        pickle.dump(phrases, f)
     
     text.close()
 
 def compute(item):
-    """Computes an answer to a given sentence, taking into account the current context"""
-    global context, phrases, phraseList
-    
-    answer = ""
+    """Computes an answer to a given sentence, taking into account the lastNode value"""
     AnalyzeSentence(item)
-
-    if not phrases.__contains__(item):
-        phrases.append(item)
-        with open("phrases", "wb") as f:
-            pickle.dump(phrases, f)
-    
-    idx = phrases.index(item)
-    context = context[1:] + [idx,]
-
-    found = False
-    for i in range(5):
-        c = "%d|%d|%d|%d|%d" % tuple(([-1,] * 5)[0:i] + context[i:5])
-
-        if phraseList.__contains__(c):
-            found = True
-            if phraseList[c].__contains__(idx):
-                phraseList[c][idx] += 1
-            else:
-                phraseList[c][idx] = 1
-            
-            lst = list(phraseList[c].keys())
-            idx = phraseList[c][lst[random.randint(0, len(lst)-1)]]
-            answer = phrases[idx]
-            context = context[1:] + [idx,]
-
-            foundRep = False
-            for j in range(5):
-                c = "%d|%d|%d|%d|%d" % tuple(([-1,]*5)[0:j] + context[j:5])
-                if phraseList.__contains__(c):
-                    foundRep = True
-                    if phraseList[c].__contains__(idx):
-                        phraseList[c][idx] += 1
-                    else:
-                        phraseList[c][idx] = 1
-                    break
-
-            if not foundRep:
-                for j in range(5):
-                    c = "%d|%d|%d|%d|%d" % tuple(([-1,]*5)[0:j] + context[j:5])
-                    phraseList[c] = {idx:1}
-                with open("nodes-phrases", "wb") as f:
-                    pickle.dump(phraseList, f)
-                break
-
-    if not found:
-        item = computeRandomSentence()
-        if not phrases.__contains__(item):
-            phrases.append(item)
-            with open("phrases", "wb") as f:
-                pickle.dump(phrases, f)
-
-        idx = phrases.index(item)
-        answer = item
-        context = context[1:] + [idx,]
-
-        foundRep = False
-        for i in range(5):
-            c = "%d|%d|%d|%d|%d" % tuple(([-1,]*5)[0:i] + context[i:5])
-            if phraseList.__contains__(c):
-                foundRep = True
-                if phraseList[c].__contains__(idx):
-                    phraseList[c][idx] += 1
-                else:
-                    phraseList[c][idx] = 1
-                break
-
-        if not foundRep:
-            for i in range(5):
-                c = "%d|%d|%d|%d|%d" % tuple(([-1,]*5)[0:i] + context[i:5])
-                phraseList[c] = {idx:1}
-            with open("nodes-phrases", "wb") as f:
-                pickle.dump(phraseList, f)
-
-    return answer
+    return computeRandomSentence(True)
 
 def dumpGraph(filePath):
     """Dump graph to a file"""
@@ -219,6 +151,12 @@ def dumpGraph(filePath):
 
             if right[-4:] == " END":
                 graphFile.write('\t"%s" [color="#af3f3f", style=filled];\n' % right)
+
+                # write next node candidates
+                if mots.__contains__(item):
+                    for nextItem in mots[item]:
+                        graphFile.write('\t"%s" -> "%s" [color="#7f7f7f"]\n' % (right, (" ".join(nextItem.split("|")))).replace("\\", "\\\\").replace('"', '\\"'))
+
 
             graphFile.write('\t"%s" -> "%s" [label="%d"];\n' % (left, right, mots[key][item]))
 
