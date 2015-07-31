02c3b80b4de7b4ac597f1a5895e131213294754a
Adrien Sohier <adrien.sohier@art-software.fr>
2015-07-31 15:48:59 +0200

    fixed sort error

--- updateIcs.sh
+++ updateIcs.sh
@@ -4,7 +4,7 @@ curl -s https://www.google.com/calendar/ical/bronycub%40gmail.com/public/basic.i
 	tr -d "\r\n" |\
 	sed "s/END:VEVENT/&\n/g" |\
 	sed "s/^.*DTSTART:\(.*\)DTEND.*LOCATION:\(.*\)SEQUENCE.*SUMMARY:\(.*\)TRANSP:.*$/\1 \3 (\2)/g" |\
-	sort -ur |\
+	sort -u |\
 	grep "^[0-9TZ]* .\+$" |\
 	while read i; do 
 		let t=$(date +%s -d "${i:0:4}-${i:4:2}-${i:6:2} ${i:9:2}:${i:11:2} +0000");
