730a7e3a440718bc1200d882f82b2ead8aa3b15f
Adrien Sohier <adrien.sohier@art-software.fr>
2015-10-12 12:55:19 +0200

    Modif ics script for SugarCUB agenda

--- updateIcs.sh
+++ updateIcs.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
-let now=$(date +%s);
-curl -s https://www.google.com/calendar/ical/bronycub%40gmail.com/public/basic.ics |\
-	tr -d "\r\n" |\
-	sed "s/END:VEVENT/&\n/g" |\
-	sed "s/^.*DTSTART:\(.*\)DTEND.*LOCATION:\(.*\)SEQUENCE.*SUMMARY:\(.*\)TRANSP:.*$/\1 \3 (\2)/g" |\
-	sort -u |\
-	grep "^[0-9TZ]* .\+$" |\
-	while read i; do 
-		let t=$(date +%s -d "${i:0:4}-${i:4:2}-${i:6:2} ${i:9:2}:${i:11:2} +0000");
-		if [ $t -gt $now ]; then
-			echo "$t ${i:17}";
-		fi;
-	done
+while true; do
+	echo "Fetching incoming events ($(date +%F %T))"
+	let now=$(date +%s);
+	curl -s http://sugarcub.bronycub.org/agenda/ics |\
+		tr -d "\r\n" |\
+		sed "s/END:VEVENT/&\n/g" |\
+		sed "s/^.*DTSTART:\(.*\)DTEND.*SUMMARY:\(.*\)DESCRIPTION:\(.*\)UID.*$/\1 \2: \3/g" |\
+		sort -u |\
+		grep "^[0-9TZ]* .\+$" |\
+		while read i; do 
+			let t=$(date +%s -d "${i:0:4}-${i:4:2}-${i:6:2} ${i:9:2}:${i:11:2} +0000");
+			if [ $t -gt $now ]; then
+				echo "$t ${i:17}";
+			fi;
+		done |\
+			cat - events.lst |\
+			sort -u > events.lst.tmp
+	mv events.lst{.tmp,}
+	echo -e "$(cat events.lst)\n"
+	sleep 5m
+done
